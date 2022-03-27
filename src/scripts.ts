4248d9700d34e486859704cd6eadfafdda0dcebe
Locria Cyber <74560659+locriacyber@users.noreply.github.com>
2022-03-27 22:22:43 +0800

    Add script

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,10 +1,10 @@
-import { readFile, createWriteStream } from 'fs'
-import glob from 'glob';
-import { relative } from 'path'
-import { isCjsFile, generateTransformer } from './utils/base'
-import { cjs2esmVisitors } from './visitors';
+import { readFile, createWriteStream } from "fs"
+import glob from "glob"
+import { relative } from "path"
+import { isCjsFile, generateTransformer } from "./utils/base"
+import { cjs2esmVisitors } from "./visitors"
 
-const transformFileBase = generateTransformer(cjs2esmVisitors);
+const transformFileBase = generateTransformer(cjs2esmVisitors)
 const ROOT = process.cwd()
 
 function _transformFiles(pattern) {
@@ -12,24 +12,36 @@ function _transformFiles(pattern) {
     if (err) {
       console.error(err)
     } else {
-      files && files.forEach(file => {
-        const filename = relative(ROOT, file,)
-        readFile(filename, (err, content) => {
-          if (content && isCjsFile(content.toString())) {
-            const str = content.toString();
-            const { code } = transformFileBase(str)
-            createWriteStream(filename).write(code)
-          }
+      files &&
+        files.forEach((file) => {
+          const filename = relative(ROOT, file)
+          readFile(filename, (err, content) => {
+            if (content && isCjsFile(content.toString())) {
+              const str = content.toString()
+              const { code } = transformFileBase(str)
+              createWriteStream(filename).write(code)
+            }
+          })
         })
-      })
     }
   })
 }
 
 export function transformFiles(paths: string | string[]) {
   if (Array.isArray(paths)) {
-    paths.forEach(v => _transformFiles(v))
+    paths.forEach((v) => _transformFiles(v))
   } else {
     _transformFiles(paths)
   }
+}
+
+export function transformFile(path) {
+  const filename = relative(ROOT, path)
+  readFile(filename, (err, content) => {
+    if (content && isCjsFile(content.toString())) {
+      const str = content.toString()
+      const { code } = transformFileBase(str)
+      createWriteStream(filename).write(code)
+    }
+  })
 }
